17d1ab0afce0b8f7a14e07f975b3febb6f7b1d9c
Amirshah98760 <amirshah98760@gmail.com>
2026-02-28 23:20:03 +0500

    Add the user post api

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ const express = require('express');
 const app = express();
 
 
+app.use(express.json());
+
+let users = [
+  { id: 1, name: "Alice", email: "alice@example.com" },
+  { id: 2, name: "Bob", email: "bob@example.com" }
+];
+
+// 1️CREATE - Add new user
+app.post("/users", (req, res) => {
+  const { name, email } = req.body;
+
+  if (!name || !email) {
+    return res.status(400).json({ message: "Name and email required" });
+  }
+
+  const newUser = {
+    id: users.length ? users[users.length - 1].id + 1 : 1,
+    name,
+    email
+  };
+
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
+
+
 
 
 
